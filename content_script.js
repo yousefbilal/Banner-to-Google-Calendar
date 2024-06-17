0f2d8b0d85fba2d736175af69a13451294338920
Yousef Irshaid <yousefbilal17@gmail.com>
2024-06-17 22:56:01 +0400

    formatted files

--- content_script.js
+++ content_script.js
@@ -1,71 +1,72 @@
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    // console.log('content_script.js received message');
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    // console.log(request);
-    if (request.message === 'retrieve_table_data') {
-
-        const data = parseTimetable();
-        sendResponse({ elems: data });
-    } else {
-        sendResponse({elems: []});
-    }
+  // console.log('content_script.js received message');
+  // console.log(request);
+  if (request.message === "retrieve_table_data") {
+    const data = parseTimetable();
+    sendResponse({ elems: data });
+  } else {
+    sendResponse({ elems: [] });
+  }
 });
 
-
 const parseTimetable = () => {
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const timetable = frames[1].document.querySelector(".datadisplaytable");
 
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    const timetable = frames[1].document.querySelector('.datadisplaytable');
-    
-    const rows = Array.from(timetable.querySelectorAll('tr'));
-    rows.shift(); 
-    let courses = {};
-    let paddingTDs = [];
+  const rows = Array.from(timetable.querySelectorAll("tr"));
+  rows.shift();
+  let courses = {};
+  let paddingTDs = [];
 
-    rows.forEach((row, i) => {
-        const cells = row.querySelectorAll('td');
+  rows.forEach((row, i) => {
+    const cells = row.querySelectorAll("td");
 
-        cells.forEach((cell, index) => {
-            if (cell.innerHTML.trim() === '&nbsp;')
-                return 
-            
-            let [course, crn, time, location] = cell.innerText.split('\n');
-            let day = days[index]
-            const numEmptySlots = parseInt(cell.getAttribute('rowspan')) - 1;
-            for (let j = 1; j <= numEmptySlots; j++) {
-                const nextRow = rows[i + j];
-                const emptyCell = document.createElement('td');
-                emptyCell.innerHTML = '&nbsp;';
-                emptyCell.style.display = 'none';
-                // check if row has a TH element, if so increase insertIndex by 1
-                let insertIndex = nextRow.querySelector('th') ? index + 1 : index;
-                nextRow.insertBefore(emptyCell, nextRow.children[insertIndex]);
-                paddingTDs.push(emptyCell);
-            }
+    cells.forEach((cell, index) => {
+      if (cell.innerHTML.trim() === "&nbsp;") return;
 
-            if (!courses[crn]) {
-                let [startTime, endTime] = time.split('-');
-                startTime = `${day} ${startTime}`;
-                endTime = `${day} ${endTime}`;
-                courses[crn] = {
-                    course,
-                    startTime,
-                    endTime,
-                    location,
-                    days: []
-                };
-            }
+      let [course, crn, time, location] = cell.innerText.split("\n");
+      let day = days[index];
+      const numEmptySlots = parseInt(cell.getAttribute("rowspan")) - 1;
+      for (let j = 1; j <= numEmptySlots; j++) {
+        const nextRow = rows[i + j];
+        const emptyCell = document.createElement("td");
+        emptyCell.innerHTML = "&nbsp;";
+        emptyCell.style.display = "none";
+        // check if row has a TH element, if so increase insertIndex by 1
+        let insertIndex = nextRow.querySelector("th") ? index + 1 : index;
+        nextRow.insertBefore(emptyCell, nextRow.children[insertIndex]);
+        paddingTDs.push(emptyCell);
+      }
 
-            courses[crn].days.push(day);
-            
-        });
-    })
+      if (!courses[crn]) {
+        let [startTime, endTime] = time.split("-");
+        startTime = `${day} ${startTime}`;
+        endTime = `${day} ${endTime}`;
+        courses[crn] = {
+          course,
+          startTime,
+          endTime,
+          location,
+          days: [],
+        };
+      }
 
-    // Remove padding tds
-    paddingTDs.forEach(paddingTD => {
-        paddingTD.remove();
+      courses[crn].days.push(day);
     });
+  });
 
-    return Object.values(courses);
-}
+  // Remove padding tds
+  paddingTDs.forEach((paddingTD) => {
+    paddingTD.remove();
+  });
+
+  return Object.values(courses);
+};
--- popup.js
+++ popup.js
@@ -13,110 +13,99 @@ import moment from "moment";
 //         }
 //     }
 
-    
 //     let newToken;
 //     try {
 //         newToken = await chrome.identity.getAuthToken({ 'interactive': true });
 //     } catch (error) {
 //         throw new Error('Failed to obtain token');
-//     }   
+//     }
 
 //     token = newToken.token;
 //     localStorage.setItem('authToken', token);
 
 //     const expiresIn = 1800
 
-//     const expiration = Date.now() + expiresIn * 1000; 
+//     const expiration = Date.now() + expiresIn * 1000;
 //     localStorage.setItem('authTokenExpiration', expiration.toString());
 
 //     return token;
 // };
-const manifest = chrome.runtime.getManifest()
+const manifest = chrome.runtime.getManifest();
 const REDIRECT_URL = chrome.identity.getRedirectURL();
 // client ID of the Web Application and NOT the chrome extension
-const CLIENT_ID = "85232819736-kvha7463v3n834f8s2gfjqfrsoefhhm0.apps.googleusercontent.com";
+const CLIENT_ID =
+  "85232819736-kvha7463v3n834f8s2gfjqfrsoefhhm0.apps.googleusercontent.com";
 const SCOPES = manifest.oauth2.scopes;
-const AUTH_URL =
-`https://accounts.google.com/o/oauth2/auth\
+const AUTH_URL = `https://accounts.google.com/o/oauth2/auth\
 ?client_id=${CLIENT_ID}\
 &response_type=token\
 &redirect_uri=${encodeURIComponent(REDIRECT_URL)}\
-&scope=${encodeURIComponent(SCOPES.join(' '))}`;
-
+&scope=${encodeURIComponent(SCOPES.join(" "))}`;
 
 const parseResponse = (responseUri) => {
-    let responseParams = responseUri.split('#')[1];
-    responseParams = new URLSearchParams(responseParams);
-    let token = responseParams.get('access_token');
-    let expiresIn = responseParams.get('expires_in');
-    return { token, expiresIn };
-
-}
+  let responseParams = responseUri.split("#")[1];
+  responseParams = new URLSearchParams(responseParams);
+  let token = responseParams.get("access_token");
+  let expiresIn = responseParams.get("expires_in");
+  return { token, expiresIn };
+};
 
 const getAuthToken = async () => {
-    try {
-        let responseUri = await chrome.identity.launchWebAuthFlow({
-            interactive: true,
-            url: AUTH_URL
-        });
+  try {
+    let responseUri = await chrome.identity.launchWebAuthFlow({
+      interactive: true,
+      url: AUTH_URL,
+    });
 
-        if(!responseUri) {
-            throw new Error('Failed to obtain token');
-        }
-
-        const {token, expiresIn} = parseResponse(responseUri);
-        console.log('token', token)
-        console.log('expiresIn', expiresIn)
-        if (!token || token.length < 1)
-            throw new Error('Failed to obtain token');
-
-    } catch (error) {
-        console.error(error);
-        throw new Error('Failed to obtain token');
+    if (!responseUri) {
+      throw new Error("Failed to obtain token");
     }
-    
 
-
-}
-
-
-
+    const { token, expiresIn } = parseResponse(responseUri);
+    console.log("token", token);
+    console.log("expiresIn", expiresIn);
+    if (!token || token.length < 1) throw new Error("Failed to obtain token");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to obtain token");
+  }
+};
 
 const createCalendar = async (calendarName, headers) => {
-    let res = await fetch('https://www.googleapis.com/calendar/v3/calendars', {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify({
-            summary: calendarName
-        })
-    });
-    if (!res.ok) {
-        console.error('Failed to create calendar:', res.status);
-        throw new Error('Failed to create calendar');
-    }
+  let res = await fetch("https://www.googleapis.com/calendar/v3/calendars", {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify({
+      summary: calendarName,
+    }),
+  });
+  if (!res.ok) {
+    console.error("Failed to create calendar:", res.status);
+    throw new Error("Failed to create calendar");
+  }
 
-    return await res.json();
-}
+  return await res.json();
+};
 
 const createSchedule = async (event) => {
-    event.preventDefault();
-    let data;
-    let headers
-    document.getElementById('submit').disabled = true;
-    displayMessage('Creating schedule...', 'black')
-    try {
-        const token = await getAuthToken();
-        console.log('token', token)
-        console.log(typeof token)
+  event.preventDefault();
+  let data;
+  let headers;
+  document.getElementById("submit").disabled = true;
+  displayMessage("Creating schedule...", "black");
+  try {
+    const token = await getAuthToken();
+    console.log("token", token);
+    console.log(typeof token);
     //     const calendarName = document.getElementById("textin").value;
-    
+
     //     headers = {
     //         'Authorization': `Bearer ${token}`,
     //         'Content-Type': 'application/json'
     //     };
 
     //     data = await createCalendar(calendarName, headers);
-            
+
     //     const tableData = await retrieveTableData();
     //     let promises = tableData.map((eventData, index) => insertEvent(data.id, headers, eventData, index % 11 + 1))
     //     let results = await Promise.all(promises);
@@ -127,105 +116,104 @@ const createSchedule = async (event) => {
     //         await deleteCalendar(data.id, headers);
     //     console.error(error);
     //     displayMessage(error, 'red');
-    } finally {
-        document.getElementById('submit').disabled = false;
-    }
-}
+  } finally {
+    document.getElementById("submit").disabled = false;
+  }
+};
 
 const deleteCalendar = async (calendarName, headers) => {
-
-    let res = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarName}`, {
-        method: 'DELETE',
-        headers: headers
-    });
-
-    if (!res.ok) {
-        console.error('Failed to delete calendar:', res.status);
+  let res = await fetch(
+    `https://www.googleapis.com/calendar/v3/calendars/${calendarName}`,
+    {
+      method: "DELETE",
+      headers: headers,
     }
+  );
 
-    return await res.text();
-}
+  if (!res.ok) {
+    console.error("Failed to delete calendar:", res.status);
+  }
+
+  return await res.text();
+};
 
 const insertEvent = async (calendarName, headers, eventData, colorId) => {
-    
-    const dayMapping = {
-        'Monday': 'MO',
-        'Tuesday': 'TU',
-        'Wednesday': 'WE',
-        'Thursday': 'TH',
-        'Friday': 'FR',
-        'Saturday': 'SA',
-        'Sunday': 'SU'
-    };
+  const dayMapping = {
+    Monday: "MO",
+    Tuesday: "TU",
+    Wednesday: "WE",
+    Thursday: "TH",
+    Friday: "FR",
+    Saturday: "SA",
+    Sunday: "SU",
+  };
 
-    let formattedDays = eventData.days.map(day => dayMapping[day]).join(',');
-    let startTime = moment(eventData.startTime, "dddd h:mm a").toISOString();
-    let endTime = moment(eventData.endTime, "dddd h:mm a").toISOString();
-    const body = {
-        summary: eventData.course,
-        location: eventData.location,
-        start: {
-            dateTime: startTime,
-            timeZone: 'Asia/Dubai'
-        },
-        end: {
-            dateTime: endTime,
-            timeZone: 'Asia/Dubai'
-        },
-        colorId: colorId,
-        recurrence: [
-            'RRULE:FREQ=WEEKLY;BYDAY=' + formattedDays
-        ]
-    };
+  let formattedDays = eventData.days.map((day) => dayMapping[day]).join(",");
+  let startTime = moment(eventData.startTime, "dddd h:mm a").toISOString();
+  let endTime = moment(eventData.endTime, "dddd h:mm a").toISOString();
+  const body = {
+    summary: eventData.course,
+    location: eventData.location,
+    start: {
+      dateTime: startTime,
+      timeZone: "Asia/Dubai",
+    },
+    end: {
+      dateTime: endTime,
+      timeZone: "Asia/Dubai",
+    },
+    colorId: colorId,
+    recurrence: ["RRULE:FREQ=WEEKLY;BYDAY=" + formattedDays],
+  };
 
-    let res = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarName}/events`, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(body)
-    });
-
-    if (!res.ok) {
-        console.error('Failed to insert event:', res.status);
-        throw new Error('Failed to insert event');
+  let res = await fetch(
+    `https://www.googleapis.com/calendar/v3/calendars/${calendarName}/events`,
+    {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(body),
     }
+  );
 
-    return await res.json();
+  if (!res.ok) {
+    console.error("Failed to insert event:", res.status);
+    throw new Error("Failed to insert event");
+  }
 
-}
+  return await res.json();
+};
 
+const retrieveTableData = async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  const response = await chrome.tabs.sendMessage(tab.id, {
+    message: "retrieve_table_data",
+  });
 
-const retrieveTableData = async () =>  {
-    
-    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-    const response = await chrome.tabs.sendMessage(tab.id, { message: 'retrieve_table_data' });
-    
-    // console.log('response', response)
-    if (!response.elems) {
-        console.error('Failed to retrieve table data');
-        return;
-    }
+  // console.log('response', response)
+  if (!response.elems) {
+    console.error("Failed to retrieve table data");
+    return;
+  }
 
-    return response.elems
-}
-
+  return response.elems;
+};
 
 // create a function that displays the error on the DOM
 const displayMessage = (message, color) => {
-    const messageDiv = document.getElementById('message');
-    messageDiv.textContent = message;
-    messageDiv.style.display = 'block';
-    messageDiv.style.color = color;
-    messageDiv.style.border = '2px solid';
-    messageDiv.style.borderRadius = '10px';
-    messageDiv.style.borderColor = color;
-    messageDiv.style.padding = '5px';
-    messageDiv.style.fontSize = '10pt';
-    
-    setTimeout(() => {
-        messageDiv.textContent = '';
-        messageDiv.style.display = 'none';
-    }, 5000);
+  const messageDiv = document.getElementById("message");
+  messageDiv.textContent = message;
+  messageDiv.style.display = "block";
+  messageDiv.style.color = color;
+  messageDiv.style.border = "2px solid";
+  messageDiv.style.borderRadius = "10px";
+  messageDiv.style.borderColor = color;
+  messageDiv.style.padding = "5px";
+  messageDiv.style.fontSize = "10pt";
 
-}
+  setTimeout(() => {
+    messageDiv.textContent = "";
+    messageDiv.style.display = "none";
+  }, 5000);
+};
 
-document.getElementById('form').onsubmit = createSchedule;
+document.getElementById("form").onsubmit = createSchedule;
